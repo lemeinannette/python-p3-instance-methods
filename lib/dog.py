89fe71a0ff193ca9a5bc0d83f5a562caece02d65
Lemein <lemeinannette@gmail.com>
2023-11-29 11:06:18 +0300

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,15 @@
 
 class Dog:
     # Class body goes here
-
+   
     #Instance method definition
-    pass
+    def bark (self):
+      print("Woof!")
+
+      # instance method 2
+    def sit (self):
+         print ("The dog is sitting.")
+fido = Dog ()
+fido.bark()
+fido.sit()
+print(fido)
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,14 @@
 
 class Person:
     # Class body goes here
-
+   def talk(self):
+     print ("Hello World!")
     #Instance method definition
-    pass
+
+   def walk(self): 
+    print ("The person is walking.")
+
+
+
+
+
